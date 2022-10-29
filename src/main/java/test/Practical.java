4e107892dab6ddbb9815eb9653aed2cca4ebbffc
ALexandr <vinogradovsacha@mail.ru>
2022-10-29 17:49:32 +0300

    решение задачи 1.3

---
+++ src/main/java/test/Practical.java
@@ -0,0 +1,12 @@
+package test;
+
+import java.util.Scanner;
+
+public class Practical {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        String num = scan.nextLine();
+      System.out.println("You entered a number: " + num);
+    }
+}
