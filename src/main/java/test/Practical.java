65614e237e2f9d8bedd3bece7c9be2f81ef8342e
ALexandr <vinogradovsacha@mail.ru>
2022-11-02 01:08:17 +0300

    ch02n013

--- src/main/java/test/Practical.java
+++ src/main/java/test/Practical.java
@@ -1,12 +1,16 @@
 package test;
-
-import java.util.Scanner;
-
 public class Practical {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter the number: ");
-        String num = scan.nextLine();
-      System.out.println("You entered a number: " + num);
+    public static void main (String[]args){
+        int x = 189;
+        int a = 0;
+        for (int i = 0; i < 3; i++) {
+            a *= 10;
+            a += x % 10;
+            x /= 10;
+        }
+        System.out.println(a);
+
+        }
     }
-}
+
+
